db15d754bceaeb7aea62204cb47197e038ca0af4
Matthijs4004 <matthijs4004@gmail.com>
2022-03-24 11:45:55 +0100

    simpel exception.py programma toegevoegd

---
+++ exception.py
@@ -0,0 +1,13 @@
+from webbrowser import get
+
+
+getal = input("Vul eens een getal hier in, maakt niet uit welk getal ")
+
+try:
+    getal = float(getal)
+except ValueError:
+    print("Gast, ik zei vul een nummer in")
+else:
+    print("Lekker bezig!")
+finally:
+    exit()
