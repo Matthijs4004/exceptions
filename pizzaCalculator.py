db6f8bd55c5f263a0a33efa49321e1d7cdba2f81
Matthijs4004 <matthijs4004@gmail.com>
2022-03-24 11:27:07 +0100

    pizzaCalculator refactored met try en except

--- pizzaCalculator.py
+++ pizzaCalculator.py
@@ -1,35 +1,63 @@
 # Matthijs Raatgever PizzaCalculator
 
+from cgitb import small
+from re import I
+
+
 largePizza = 12.00
 mediumPizza = 7.00
 smallPizza = 4.00
+smallAmount = 0 
+mediumAmount = 0
+largeAmount = 0
 
-# Dit is de kaart met alle prijzen per grootte van de pizza
+prijzen ="""-------------------------
+| Large pizza  : 12.00  |
+| Medium pizza : 7.00   |
+| Small pizza  : 4.00   |
+-------------------------"""
 
-print("-------------------------")
-print("| Large pizza  : 12.00  |")
-print("|                       |")
-print("| Medium pizza : 7.00   |")
-print("|                       |")
-print("| Small pizza  : 4.00   |")
-print("-------------------------")
+def bestel():
+    global largeAmount, mediumAmount, smallAmount
+    i = True
+    print(prijzen)
+    pizzaInput = input("Welke grootte pizza wilt u? ").lower()
+    while i:
+        try:
+            pizzaAmount = int(input(f"Hoeveel {pizzaInput} pizza's wilt u? "))
+        except ValueError:
+            print("Dat is geen geldig aantal")
+        else:
+            i = False
+    
+    if pizzaInput == "small":
+        smallAmount += pizzaAmount
+    elif pizzaInput == "medium":
+        mediumAmount += pizzaAmount
+    elif pizzaInput == "large":
+        largeAmount += pizzaAmount
 
-# Invoer van hoeveel pizza's de klant wilt hebben
+    nogmaals()
 
-hoeveelLarge = int(input("Hoeveel large pizza's wilt u? "))
-hoeveelMedium = int(input("Hoeveel medium pizza's wilt u? "))
-hoeveelSmall = int(input("Hoeveel small pizza's wilt u? "))
+def nogmaals():
+    nogmaals = input("Wilt u nog meer bestellen? ").lower()
+    if nogmaals == "ja":
+        bestel()
+    else:
+        bon()
 
-# Lijstje van hoeveel pizza's de klant heeft besteld
+def bon():
+    global largeAmount, mediumAmount, smallAmount
+    totaal = (largeAmount*largePizza) + (mediumAmount*mediumPizza) + (smallAmount*smallPizza)
+    print("---------------------------")
+    if largeAmount > 0:
+        print(f" {largeAmount}x Large Pizza   : {largeAmount*largePizza}")
+    if mediumAmount > 0:
+        print(f" {mediumAmount}x Medium Pizza : {mediumAmount*mediumPizza}")
+    if smallAmount > 0:
+        print(f" {smallAmount}x Small Pizza   : {smallAmount*smallPizza}")
+    print(f" Totaal           : {totaal}")
+    print("---------------------------")
 
-print("Hoeveelheid large pizza's: ", hoeveelLarge)
-print("Hoeveelheid medium pizza's: ", hoeveelMedium)
-print("Hoeveelheid small pizza's: ", hoeveelSmall)
 
-# De berekening van het totaal bedrag wat de klant moet betalen
-
-bedrag = (hoeveelLarge * largePizza) + (hoeveelMedium * mediumPizza) + (hoeveelSmall * smallPizza)
-
-factuurtekst = "U heeft voor " + str(bedrag) + " aan pizza's besteld."
-
-print(factuurtekst)
+bestel()
